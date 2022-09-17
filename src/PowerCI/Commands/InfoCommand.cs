2f704f341447e74d3e1eb574e9e60244daaa8175
Erik Xu <81636759@qq.com>
2022-09-17 23:13:12 +0800

    Add info command.

---
+++ src/PowerCI/Commands/InfoCommand.cs
@@ -0,0 +1,20 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+namespace PowerCI.Commands
+{
+    [Command("info", Description = "Get info")]
+    internal class InfoCommand
+    {
+        private readonly IConsole _console;
+
+        public InfoCommand(IConsole console)
+        {
+            _console = console;
+
+        }
+        public void OnExecute()
+        {
+            _console.WriteLine("Power CI tools");
+        }
+    }
+}
--- src/PowerCI/Program.cs
+++ src/PowerCI/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace PowerCI
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using PowerCI.Commands;
+
+namespace PowerCI
 {
+    [HelpOption(Inherited = true)]
+    [Command(Description = "Power CI tools"),
+     Subcommand(typeof(InfoCommand))]
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton(PhysicalConsole.Singleton);
+
+            var services = serviceCollection.BuildServiceProvider();
+
+            var app = new CommandLineApplication<Program>();
+            app.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection(services);
+
+            var console = (IConsole)services.GetService(typeof(IConsole));
+
+            try
+            {
+                return app.Execute(args);
+            }
+            catch (UnrecognizedCommandParsingException ex)
+            {
+                console.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        public int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            console.WriteLine("Please specify a command.");
+            app.ShowHelp();
+            return 1;
         }
     }
 }
