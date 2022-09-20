ea9e07699fbad598bfe530b72ca330f8f697d40e
Erik Xu <81636759@qq.com>
2022-09-20 22:27:20 +0800

    Modify jenkins list job command.

--- src/PowerCI/Commands/Jenkins/JenkinsJobCommand.cs
+++ src/PowerCI/Commands/Jenkins/JenkinsJobCommand.cs
@@ -1,7 +1,5 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using Newtonsoft.Json;
-using RestSharp;
-using RestSharp.Authenticators;
 
 namespace PowerCI.Commands.Jenkins
 {
@@ -15,88 +13,26 @@ namespace PowerCI.Commands.Jenkins
     }
 
     [Command("list", Description = "List jenkins jobs")]
-    internal class JenkinsJobListCommand
+    internal class JenkinsJobListCommand : JenkinsParameter
     {
-        [Option(Description = "Jenkins Host", ShortName = "H")]
-        public string? Host { get; set; }
+        [Option(Description = "Folder Name", ShortName = "f")]
+        public string? Folder { get; set; }
 
-        [Option(Description = "Jenkins User", ShortName = "u")]
-        public string? User { get; set; }
-
-        [Option(Description = "Jenkins User Password", ShortName = "p")]
-        public string? Password { get; set; }
-
-        public void OnExecute(IConsole console)
+        public void OnExecute(IConsole console, IJenkinsService jenkinsService)
         {
-            var json = File.ReadAllText(JenkinsCommand.ConfigPath);
-            var configs = JsonConvert.DeserializeObject<Dictionary<string, string>>(json) ?? new Dictionary<string, string>();
-
-            var host = string.Empty;
-
-            if (Host != null)
+            var (isValid, host, user, tokenOrPassword) = ValidParameters(console);
+            if (!isValid)
             {
-                host = Host.TrimEnd('/');
-            }
-            else if (configs.ContainsKey("host"))
-            {
-                host = configs["host"];
+                return;
             }
 
-            if (host == string.Empty)
+            var (success, list) = jenkinsService.ListJob(host, user, tokenOrPassword, Folder ?? string.Empty);
+            if (!success)
             {
-                console.WriteLine("Host is required");
+                return;
             }
 
-            var user = string.Empty;
-
-            if (User != null)
-            {
-                user = User;
-            }
-            else if (configs.ContainsKey("user"))
-            {
-                user = configs["user"];
-            }
-
-            if (user == string.Empty)
-            {
-                console.WriteLine("User is required");
-            }
-
-            var password = string.Empty;
-
-            if (Password != null)
-            {
-                password = Password;
-            }
-            else if (configs.ContainsKey("password"))
-            {
-                password = configs["password"];
-            }
-
-            if (password == string.Empty)
-            {
-                console.WriteLine("Password is required");
-            }
-
-            using var client = new RestClient();
-            var request = new RestRequest
-            {
-                Method = Method.Get,
-                Resource = $"{host}/api/json?tree=jobs[name,url]&pretty=true"
-            };
-
-            client.Authenticator = new HttpBasicAuthenticator(user, password);
-            var response = client.Execute(request);
-            if (response.IsSuccessful)
-            {
-                var content = response.Content;
-                console.WriteLine(content ?? string.Empty);
-            }
-            else
-            {
-                console.WriteLine("Call jenkins api failed");
-            }
+            console.WriteLine(JsonConvert.SerializeObject(list, Formatting.Indented));
         }
     }
 }
